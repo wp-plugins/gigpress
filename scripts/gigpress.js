1d82f26447e417142502e7403f2a2d6eb3022cea
mrherbivore <mrherbivore@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-09 15:40:14 +0000

    Fixed conflict with Google Analytics plugin
    git-svn-id: https://plugins.svn.wordpress.org/gigpress/trunk@343171 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scripts/gigpress.js
+++ scripts/gigpress.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
 		return this.animate({opacity: "toggle"}, speed, easing, callback); 
 	}; 	
 	$("a.gigpress-links-toggle").click(function() {
-		var target = $(this).attr("href");
+		target = $(this).attr("href").replace(document.location.href,'');
 		$(target).fadeToggle("fast");
 		$(this).toggleClass("gigpress-link-active");
 		return false;
